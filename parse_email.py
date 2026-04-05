da3392583c395fe727a39fc2827e5039a5f46aee
rafidalhaque <rafidalhaque@proton.me>
2026-04-05 17:16:10 +0600

    refactor(parse): move StripHTML class and strip_html function before parse_email

--- parse_email.py
+++ parse_email.py
@@ -2,20 +2,6 @@ from email import message, parser, policy, contentmanager, header
 from pathlib import Path
 from html.parser import HTMLParser
 
-def parse_email(file):
-    with open(file, "rb") as f:
-        eml_data = parser.BytesParser(policy=policy.default).parse(f)
-
-    body = eml_data.get_body(preferencelist=("plain", "html"))
-
-    return {
-        "subject": eml_data["Subject"],
-        "from": eml_data["From"],
-        "to": eml_data["To"],
-        "date": eml_data["Date"],
-        "body": strip_html(body.get_content()) if body else "No body exists"
-    }
-
 class StripHTML(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -31,3 +17,18 @@ def strip_html(html):
     stripper = StripHTML()
     stripper.feed(html)
     return stripper.get_text()
+
+
+def parse_email(file):
+    with open(file, "rb") as f:
+        eml_data = parser.BytesParser(policy=policy.default).parse(f)
+
+    body = eml_data.get_body(preferencelist=("plain", "html"))
+
+    return {
+        "subject": eml_data["Subject"],
+        "from": eml_data["From"],
+        "to": eml_data["To"],
+        "date": eml_data["Date"],
+        "body": strip_html(body.get_content()) if body else "No body exists"
+    }
